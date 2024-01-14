7b501d87edd8fa990c8dc87bd85155899ff746ba
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2024-01-14 21:15:07 +0100

    creating the user schema

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-
-console.log({
-	clientId: process.env.GOOGLE_ID,
-	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
+import User from "@models/user";
+import { connectDB } from "@utils/database";
 
 const handler = NextAuth({
 	providers: [
@@ -14,17 +11,36 @@ const handler = NextAuth({
 		}),
 	],
 
-    async session({ session }) {
-        
-    },
+	async session({ session }) {
+		const sessionUser = await User.findOne({
+			email: session.user.email,
+		});
 
-    async signIn({ profile }) {
-        try {
-            
-        } catch (error) {
-            
-        }
-    },
+		session.user.id = sessionUser._id.toString();
+
+		return session;
+	},
+
+	async signIn({ profile }) {
+		try {
+			await connectDB();
+
+			const userExist = await User.findOne({ email: profile.email });
+
+			if (!userExist) {
+				await userExist.create({
+					email: profile.email,
+					username: profile.name.replace(" ", "").toLowercase(),
+					image: profile.picture,
+				});
+			}
+
+			return true;
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	},
 });
 
 export { handler as GET, handler as POST };
