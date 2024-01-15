d25530e076f1d9a2a3e1caa83c3de931e0caa5fb
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2024-01-16 00:55:01 +0100

    creating the functionality for the prompt

--- app/Create-prompt/page.jsx
+++ app/Create-prompt/page.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const Createprompt = () => {
-  return (
-    <div>Createprompt</div>
-  )
-}
+	const [submitting, setSubmitting] = useState(false);
+	const [post, setPost] = useState({
+		prompt: "",
+		tag: "",
+	});
 
-export default Createprompt
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true)
+  };
+
+	return (
+		<Form
+			type="Create"
+			post={post}
+			setPost={setPost}
+			submitting={submitting}
+			handleSubmit={createPrompt}
+		/>
+	);
+};
+
+export default Createprompt;
